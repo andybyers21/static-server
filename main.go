36e0050a7eddfc1d10706d5382bbc66358111d3c
andybyers21 <andy.byers21@gmail.com>
2021-01-17 15:24:09 +0000

    add robust error handling & templated pages test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -13,7 +14,6 @@ func main() {
 	// Register file server for all requests.
 	// Strip off the "/static/" prefix from the request path before searching the file system.
 	http.Handle("/static/", http.StripPrefix("/static", fs))
-
 	// All requests not picked up by the static file server should be handled by serveTemplates() function.
 	http.HandleFunc("/", serveTemplates)
 
@@ -29,7 +29,37 @@ func serveTemplates(w http.ResponseWriter, r *http.Request) {
 	lp := filepath.Join("templates", "layout.html")
 	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
 
+	// Return 404 if the requested template does not exist.
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	// Return 404 if the requested template path is a directory.
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(lp, fp)
+	if err != nil {
+		// Log detailed error
+		log.Println(err.Error())
+		// Return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
+
 	// Bundle the requested template and layout into a template set and render a named template in the set.
-	tmpl, _ := template.ParseFiles(lp, fp)
-	tmpl.ExecuteTemplate(w, "layout", nil)
+	// tmpl, _ := template.ParseFiles(lp, fp)
+	// tmpl.ExecuteTemplate(w, "layout", nil)
 }
