1f3b3da93f1da854b84c13c13ee070510b32a877
andybyers21 <andy.byers21@gmail.com>
2021-01-17 14:18:57 +0000

    test page links

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 func main() {
 	// Create a handler which responds to all HTTP requests with the contents of a given file system (static).
 	fs := http.FileServer(http.Dir("./static"))
-	// Register file server for all requests (on port 9100 in this case).
-	http.Handle("/", fs)
+	// Register file server for all requests.
+	// Strip off the "/static/" prefix from the request path before searching the file system
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 
 	log.Println("Listening on port 9100...")
 	err := http.ListenAndServe(":9100", nil)
